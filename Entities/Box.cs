acf49e6daebd119dbe12d05f5e3caf2478368049
AndersJakobsen <ande12n3@easv365.dk>
2022-10-13 08:51:40 +0200

    failed push

---
+++ Entities/Box.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities;
+
+public class Box
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int Length { get; set; }
+    public int Width { get; set; }
+}
